bb92cf29a22f15b5f8a0bb884a75e76bfab5c5a1
coderDangi190 <sandangip920@gmail.com>
2025-05-27 10:01:46 +0545

    initial_commit

---
+++ Core_java/Data_type.java
@@ -0,0 +1,25 @@
+public class Data_type {
+    public static void main(String[] args) {
+        short myNum = 39;
+        System.out.println("Short value: " + myNum);
+        // Integer data type
+        int myInt = 100;
+        System.out.println("Integer value: " + myInt);
+
+        // Floating-point data type
+        float myFloat = 5.99f;
+        System.out.println("Float value: " + myFloat);
+
+        // Character data type
+        char myChar = 'A';
+        System.out.println("Character value: " + myChar);
+
+        // Boolean data type
+        boolean myBool = true;
+        System.out.println("Boolean value: " + myBool);
+
+        // String data type
+        String myString = "Hello, World!";
+        System.out.println("String value: " + myString);
+    }
+}
---
+++ Core_java/Sandip.java
@@ -0,0 +1,36 @@
+
+public class Sandip{
+    public static void main(String[] args){
+        // System.out.println("Good morning, Sandip!");
+       /* int myNum = 15;
+            myNum = 32;
+            System.out.println(myNum);  */
+
+        /* Print Variable
+        String firstName = "Sandip";
+        String lastName = "Dangi";
+        String fullName = firstName + " "+ lastName;
+        System.out.println("My name is "+ fullName); 
+        */
+
+        /* Multiple Variables 
+            int a =9 , b = 3, c =1;
+            int sum = a + b + c;
+            System.out.println("The sum of a, b and c is: " + sum); 
+        */
+
+        // Real life example
+        String studentName = "Sandip Dangi";
+        int studentId = 123;
+        int studentAge = 20;
+        float studentFee = 1500.50f;
+        char studentGrade = 'A';
+
+        System.out.println("Student Name: " + studentName);
+        System.out.println("Student ID: " + studentId);
+        System.out.println("Student Age: " + studentAge);
+        System.out.println("Student Fee: " + studentFee);
+        System.out.println("Student Grade: " + studentGrade);             
+        
+    }
+}
